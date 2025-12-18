8e0e066a0b5bb34bddaaec74d98dc100cfa725b6
Sharjeel-op <sharjeelsheikh424@gmail.com>
2025-12-19 04:40:18 +0500

    Update production URL in docusaurus.config.js

--- book-site/docusaurus.config.js
+++ book-site/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://Sharjeel-op/ai-book.vercel.app',
+  url: 'https://ai-book.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl:'/',
